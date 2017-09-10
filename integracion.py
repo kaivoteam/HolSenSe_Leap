f3526f907177e3dff956bbcbc233cb0853c0d8cd
F4BI4NFF <fabianff95@hotmail.com>
2017-09-10 00:40:14 -0300

    Ajustes movimientos , alternativa para zoom

--- integracion.py
+++ integracion.py
@@ -32,10 +32,10 @@ class SampleListener(Leap.Listener):
     state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
     min_largo = 200.0
     min_velocidad = 50
-    x_prev = 0.0
-    y_prev= 0.0
-    app_x = 0.0
-    app_y = 0.0
+    #x_prev = 0.0
+    #y_prev= 0.0
+    #app_x = 0.0
+    #app_y = 0.0
 
     def on_init(self, controller):
         print "Initialized"
@@ -69,9 +69,26 @@ class SampleListener(Leap.Listener):
         #print "Frame id1: %d, Frame id2: %d, Frameid3: %d, Frameid4: %d" % (
               #frame.id, previous.id, previous2.id, previous3.id)
         
-        
-        #ZOOM IN Y ZOOM OUT!!
         print "CUANTAS MANOS HAY antes del for:",len(frame.hands)
+        for hand in previous.hands:
+            manos = len(frame.hands)
+            if(manos == 1):
+                continue
+            gesto = "zoom"
+            ## 
+            handType = "Left hand" if hand.is_left else "Right hand"
+            # CAJA 
+            interaction_box = frame.interaction_box
+            #Reducir tamanno de caja de interaccion!
+            app_width = 700
+            app_height = 500
+            i_box = frame.interaction_box
+            normalized_tip = i_box.normalize_point(hand.palm_position)
+            x_prev = app_width  * normalized_tip.x
+            y_prev = app_height * (1 - normalized_tip.y)
+            break
+
+            
         for hand in frame.hands:
             #print "posicion palma:",hand.palm_position
             manos = len(frame.hands)
@@ -83,31 +100,49 @@ class SampleListener(Leap.Listener):
             # CAJA 
             interaction_box = frame.interaction_box
             #Reducir tamanno de caja de interaccion!
-            app_width = 300
+            app_width = 700
             app_height = 500
             i_box = frame.interaction_box
             normalized_tip = i_box.normalize_point(hand.palm_position)
-            self.app_x = app_width  * normalized_tip.x
-            self.app_y = app_height * (1 - normalized_tip.y)
-            print "X es: %f , Y es: %f " % (self.app_x, self.app_y )
-            print hand.palm_velocity.x
-            print hand.palm_velocity.y           
-        
+            app_x = app_width  * normalized_tip.x
+            app_y = app_height * (1 - normalized_tip.y)
+            print "Segun el frame anterior => X es :%f, Y es: %f" % (x_prev,y_prev)
+            print "Segun la caja => X es: %f , Y es: %f " % (app_x,app_y)
+            
+            delta_x = app_x - x_prev
+            delta_y = app_y - y_prev
+            print "El delta en X es: %f, en Y es: %f" % (delta_x, delta_y)
+            if delta_x > 10 : #in / out
+                    hacer("3",0.5,"")
+                    print "HICISTE ZOOM IN!!"
+            elif(delta_x < -20): ## CORREGIR
+                    hacer("4",0.5,"")
+                    print "HICISTE ZOOM OUT!!"
+            break
+
+            
+
+
+            #Metodo anterior...
+            
+            #print hand.palm_velocity.x
+            #print hand.palm_velocity.y           
+            """
             if cont==1: 
                 delta_x = self.app_x - self.x_prev
                 delta_y = self.app_y - self.y_prev
-                print "el cambio en X es: %f, en Y es: %f" % (delta_x, delta_y)
+                print "El delta en X es: %f, en Y es: %f" % (delta_x, delta_y)
                 self.x_prev=self.app_x
                 self.y_prev=self.app_y
-                if delta_x > 20 : #in / out
-                    hacer("3",0.1)
+                if delta_x > 40 : #in / out
+                    hacer("3",0.5,"Esta cantidad de zoom no esta permitida!")
                     print "ZOOM IN"
                 elif(delta_x < -10): ## CORREGIR
-                    hacer("4",-0.1)
+                    hacer("4",0.5,"Imagen muy pequenna")
                     print "HICISTE ZOOM OUT!!"
                 cont += 1
             break
-   
+            """
 
         for gesture in frame.gestures():
         # SWIPES!      
@@ -117,17 +152,20 @@ class SampleListener(Leap.Listener):
                 print "GIRO SWIP DETECTADO!"
                 swipe = SwipeGesture(gesture)
                 pos_swipe = swipe.position
-                speed_swipe = swipe.speed/100
+                
+                #Antes era / frames
+                speed_swipe = swipe.speed/10
+
                 print "velocidad del swipe: ", speed_swipe
-                # Cantidad de frames a girar, es proporcional a la velocidad de swipe
+                # Cantidad de frames a girar de escala 0 a 100
                 if(swipe.direction.x < 0 and self.state_names[gesture.state] == "STATE_START"):
                     print "Direccion Swip : Derecha a izquierda!"
                     print "Pos_swipe:",pos_swipe
-                    hacer("2",int(speed_swipe),"")
+                    hacer("2",0.25,"")
                 elif(swipe.direction.x > 0 and self.state_names[gesture.state] == "STATE_START"):
                     print "Direccion Swip : Izquierda a Derecha!"
                     print "Pos_swipe:",pos_swipe
-                    hacer("1",-int(speed_swipe),"")                
+                    hacer("1",0.25,"")                
                 #print " Swipe id: %d, state: %s, position: %s, direction: %s, speed: %f"% (gesture.id, self.state_names[gesture.state],swipe.position, swipe.direction, swipe.speed)
                 print "Swipe direction:",swipe.direction.x
                 break
@@ -136,34 +174,41 @@ class SampleListener(Leap.Listener):
             
             if gesture.type == Leap.Gesture.TYPE_CIRCLE:
                 circle = CircleGesture(gesture)
-                print "CIIIIRCULOOOOOOOOOOOOOOOOOOOOOO"
-                gesto= "circle"
-                if circle.pointable.direction.angle_to(circle.normal) <= Leap.PI/2:
+                print "CIIIIIRCULOOOOOOOOO"
+                # Determine clock direction using the angle between the pointable and the circle normal
+                if (circle.pointable.direction.angle_to(circle.normal) <= Leap.PI/2 and self.state_names[gesture.state] == "STATE_START"):
                     clockwiseness = "clockwise"
-                    print("sentido horario")
-                    hacer("5",30)
-                    break
-                else:
-                    clockwiseness = "counterclockwise" 
-                    print("sentido anti-horario")
-                    hacer("6",-30)
-                    break
-
+                    hacer("5")
+                break
                 """
-                sentido = clockwiseness
+                
+                previous_update = CircleGesture(controller.frame(1).gesture(circle.id))
                 swept_angle = 0
+                clockwiseness = "hola"
                 if circle.state != Leap.Gesture.STATE_START:
                     previous_update = CircleGesture(controller.frame(1).gesture(circle.id))
                     swept_angle =  (circle.progress - previous_update.progress) * 2 * Leap.PI
-                    var = swept_angle * Leap.RAD_TO_DEG
-                #print "  Circle id: %d, %s, progress: %f, radius: %f, angle: %f degrees, %s" % (
-                #        gesture.id, self.state_names[gesture.state],
-                #        circle.progress, circle.radius, swept_angle * Leap.RAD_TO_DEG, clockwiseness)
-                angulo = swept_angle * Leap.RAD_TO_DEG
-                velocidad = circle.radius
-                """
+
+                print "  Circle id: %d, %s, progress: %f, radius: %f, angle: %f degrees, %s" % (
+                        gesture.id, self.state_names[gesture.state],
+                        circle.progress, circle.radius, swept_angle * Leap.RAD_TO_DEG, clockwiseness)
+                """    
                 
             
+
+            """
+            sentido = clockwiseness
+            swept_angle = 0
+            if circle.state != Leap.Gesture.STATE_START:
+                previous_update = CircleGesture(controller.frame(1).gesture(circle.id))
+                swept_angle =  (circle.progress - previous_update.progress) * 2 * Leap.PI
+                var = swept_angle * Leap.RAD_TO_DEG
+            #print "  Circle id: %d, %s, progress: %f, radius: %f, angle: %f degrees, %s" % (
+            #        gesture.id, self.state_names[gesture.state],
+            #        circle.progress, circle.radius, swept_angle * Leap.RAD_TO_DEG, clockwiseness)
+            angulo = swept_angle * Leap.RAD_TO_DEG
+            velocidad = circle.radius
+            """
     def state_string(self, state):
         if state == Leap.Gesture.STATE_START:
             return "STATE_START"
